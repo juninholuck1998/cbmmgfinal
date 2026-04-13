49cf5ebfa6339fca7231597a7dc81a47ea0a792c
Francisco Luciano <89709261+juninholuck1998@users.noreply.github.com>
2026-04-13 15:12:13 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='cbmmg-final-v1';
+const ASSETS=['./index.html','./manifest.json'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{if(res&&res.status===200){const cl=res.clone();caches.open(CACHE).then(c=>c.put(e.request,cl));}return res;}).catch(()=>caches.match('./index.html'))));});
